e9b984f7245f56cdaed719c608c55ba25cef8a39
Rob Moore <moore.re@gmail.com>
2020-09-02 11:35:50 -0500

    Created Sensei Class in ninja.js

--- js_fundamentals/ninja.js
+++ js_fundamentals/ninja.js
@@ -8,19 +8,40 @@ class Ninja{
     
     sayName(){
         console.log(`Konnichiwa, ${this.name}`)
+        return this;
     }
 
     showStats(){
         console.log(`Name: ${this.name}; Strength: ${this.strength}; Speed: ${this.speed}; Health: ${this.health}`)
+        return this;
     }
 
     drinkSake(){
         this.health += 10
+        return this; 
+    }
+}
+
+class Sensei extends Ninja{
+    constructor(name, health, speed , strength , wisdom ){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        this.drinkSake();
+        console.log('People can spot a phony and I think young people can spot a phony')
+        return this;
     }
 }
 
 const kOmega = new Ninja("Kenny Omega", 100); 
-kOmega.sayName()
-kOmega.showStats()
-kOmega.drinkSake()
-kOmega.showStats()
+kOmega.sayName().showStats()
+kOmega.drinkSake().showStats()
+
+const tKhan = new Sensei("Tony Khan");
+tKhan.sayName().showStats()
+tKhan.speakWisdom().showStats()
