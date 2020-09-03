c94854cf17d8bc14c69148442c22179f05154cfb
Rob Moore <moore.re@gmail.com>
2020-09-03 14:57:55 -0500

    Completed Dueling Card Game Assignment

--- js_fundamentals/dueling-cards.js
+++ js_fundamentals/dueling-cards.js
@@ -12,8 +12,19 @@ class Units extends Cards{
         this.res = res; 
     }
 
+    summon(){
+        console.log(`${this.name} is in the fight!`)
+        console.table(this)
+    }
+
     attack(target){
-        target.res -= this.power; 
+        if (target instanceof Units){
+            console.log(`${this.name} attacks ${target.name}`)
+            target.res -= this.power;
+            console.table(target) 
+        } else {
+            console.log("You must attack a Unit!")
+        }
     }
 }
 
@@ -24,22 +35,37 @@ class Effects extends Cards{
         this.stat = stat; 
         this.magnitude = magnitude; 
     }
-
-    effectRes(targetUnit){
-        this.magnitude > 0 ? targetUnit.res += this.magnitude : targetUnit.res -= this.magnitude
-        return this; 
-    }
-
-    effectPower(targetUnit){
-        this.magnitude > 0 ? targetUnit.power += this.magnitude : targetUnit.power -= this.magnitude; 
-        return this; 
-    }
     
     play(target){
-        if (target instanceof Unit){
-            
+        if (target instanceof Units){
+            console.log(`${this.name} is played =>`)
+            if(this.stat == "power"){
+                target.power += this.magnitude
+                console.log(`${target.name}'s power is now ${target.power}`)
+                console.table(target)
+            } else if (this.stat == "resilience"){
+                target.res += this.magnitude
+                console.log(`${target.name}'s resilience is now ${target.res}`)
+                console.table(target)
+            }
         } else {
             throw new Error ("Target must be a unit!");
         }
     }
 }
+
+const redNinja = new Units("Red Belt Ninja", 3, 3, 4);
+const blackNinja = new Units("Black Belt Ninja", 4, 5, 4);
+const hardAlgo = new Effects("Hard Algorithm", 2, "Increase target's resilience by 3", "resilience", 3);
+const unhandledPromRej = new Effects("Unhandled Promise Rejection", 1, "Reduce target's resilience by 2", "resilience", -2);
+const pairProg = new Effects("Pair Programming", 3, "Increase target's power by 2", "power", 2)
+
+redNinja.summon(); 
+console.log('==========================================')
+hardAlgo.play(redNinja);
+console.log('==========================================')
+blackNinja.summon();
+unhandledPromRej.play(redNinja)
+console.log('==========================================')
+pairProg.play(redNinja)
+redNinja.attack(blackNinja)
