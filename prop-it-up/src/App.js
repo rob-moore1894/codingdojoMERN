f86f00ef60f3a3f2f6e0e4e3d2ce456f4b569b35
Rob Moore <moore.re@gmail.com>
2020-09-09 15:06:12 -0500

    Completed Big Inversion

--- prop-it-up/src/App.js
+++ prop-it-up/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import PersonCard from './components/personcard.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component{
   constructor(props){
--- prop-it-up/src/components/personcard.jsx
+++ prop-it-up/src/components/personcard.jsx
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 
-class PersonCard extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            age : this.props.age
-        }
-    }
-
-    addAge = () => {
-        this.setState({ age: this.state.age + 1})
-    }    
-
-    render (){
-        const { firstName, lastName, hairColor } = this.props;
-        return (
-            <div>
-                <h1>{lastName}, {firstName}</h1>
-                <h3>Age: {this.state.age}</h3>
-                <h3>Hair Color: {hairColor}</h3>
-                <button onClick={this.addAge}>Birthday Button for {firstName} {lastName}</button>
-            </div>
-        )
-    }
+const PersonCard = props => {
+    return (
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <h3>Age: {props.age}</h3>
+            <h3>Hair Color: {props.hairColor}</h3>
+        </div>
+    )
 }
+
 
 export default PersonCard; 
