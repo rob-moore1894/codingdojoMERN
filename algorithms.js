d9587ffda86b9bdcb09ebc3ffc250d5fca7078b6
Rob Moore <moore.re@gmail.com>
2020-09-15 10:00:20 -0500

    Completed Algorithms 15SEP

--- algorithms.js
+++ algorithms.js
@@ -330,3 +330,26 @@ let arr2 = [2,2,6,6,7]
 
 let intersection = arr1.filter(x => arr2.includes(x)); 
 console.log(intersection)
+
+// Efficiently combine two already-sorted arrays into a new sorted array containing the multiset union. Example: given [1,2,2,2,7] and [2,2,6,6,7], return [1,2,2,2,6,6,7].
+const unionArrays = (arr1, arr2) => {
+    var newArr = [];
+    let i = 0; 
+    let j = 0; 
+    while(i < arr1.length || j < arr2.length){
+        if(arr1[i] === arr2[j]){
+            newArr.push(arr1[i]);
+            i++
+            j++
+        } else if (arr1[i] < arr2[j]){
+            newArr.push(arr1[i]);
+            i++
+        } else {
+            newArr.push(arr2[j]);
+            j++
+        }
+    }
+    console.log(newArr)
+}
+
+unionArrays([1,2,2], [2,2,6,6,7])
