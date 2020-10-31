85d9d56258472e12106ff7ebf2938610a7bd4cab
Rob Moore <moore.re@gmail.com>
2020-10-30 21:23:48 -0500

    Added Feature: Added Validations to form

--- assignment-hook-form/src/components/UserForm.jsx
+++ assignment-hook-form/src/components/UserForm.jsx
@@ -8,6 +8,14 @@ const UserForm = (props) => {
         password: "",
         confirmPassword: "",
     })
+    
+    const formValidations = {
+        firstNameValidation: "First Name must be at least 2 characters", 
+        lastNameValidation: "Last Name must be at least 2 characters",
+        emailValidation: "Email must be at least 5 characters",
+        passwordValidation: "Password must be at least 8 characters",
+        confirmPasswordValidation: "Passwords must match",
+    }
 
     const updateInput = (e) => {
         setForm({
@@ -22,22 +30,28 @@ const UserForm = (props) => {
                 <div className="form-group">
                     <label>First Name: </label>
                     <input className = "form-control" name="firstName" onChange = {updateInput}/>
+                    {form.firstName.length > 1 ? "" : <span className="alert text-danger">{formValidations.firstNameValidation}</span>}
+                    
                 </div>
                 <div className="form-group">
                     <label>Last Name: </label>
                     <input className = "form-control" name="lastName" onChange = {updateInput}/>
+                    {form.lastName.length > 1 ? "" : <span className="alert text-danger">{formValidations.lastNameValidation}</span>}
                 </div>
                 <div className="form-group">
                     <label>Email: </label>
                     <input type="email" className = "form-control" name="email" onChange = {updateInput}/>
+                    {form.email.length > 4 ? "" : <span className="alert text-danger">{formValidations.emailValidation}</span>}
                 </div>
                 <div className="form-group">
                     <label>Password: </label>
                     <input type="password" className = "form-control" name="password" onChange = {updateInput}/>
+                    {form.password.length > 7 ? "" : <span className="alert text-danger">{formValidations.passwordValidation}</span>}
                 </div>
                 <div className="form-group">
                     <label>Confirm Password: </label>
                     <input type="password" className = "form-control" name="confirmPassword" onChange = {updateInput}/>
+                    {form.confirmPassword === form.password ? "" : <span className="alert text-danger">{formValidations.confirmPasswordValidation}</span>}
                 </div>
             </form>
 
