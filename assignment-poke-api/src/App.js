a65eb15c72c674a45faeac146bbd648963b600a8
Rob Moore <moore.re@gmail.com>
2020-11-04 18:16:17 -0600

    Refactored to use Axios for API call

--- assignment-poke-api/src/App.js
+++ assignment-poke-api/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import {useState} from 'react'; 
+import Axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
 
   const fetchPokemon = (e) => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=1050")
-        .then(response => response.json())
-        .then(response => setPokemon(response.results))
+    Axios.get("https://pokeapi.co/api/v2/pokemon?limit=1050")
+        .then(response => setPokemon(response.data.results))
     }
 
   return (
@@ -30,7 +30,7 @@ function App() {
               return(
               <tr>
                 <td>{i+1}</td>
-                <td>{poke.name}</td>
+                <td>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</td>
               </tr>
               )
             })
